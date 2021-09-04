4ed2afbe8e540d9193e608a250dd039e0b22c87d
BhavSandhu <86444297+BhavSandhu@users.noreply.github.com>
2021-09-03 18:42:03 -0700

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,11 @@
+class Drops{
+
+    constructor(){
+
+
+        
+    }
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var Man
+
+function preload(){
+    var man=loadImage("images/man.jpg",20,40,10,20)
+}
+
+function setup(){
+       
+    
+}
+
+function draw(){
+    
+    background("black")
+
+
+    display()
+    
+}   
+
